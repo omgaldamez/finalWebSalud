48936096f79134cbef8fc9213268982bd17621c7
omgaldamez <127152662+omgaldamez@users.noreply.github.com>
2023-04-11 22:44:07 -0600

    SVG Arrow
    Error en tamaño class=InfoGrafico

--- dist/js/lineas.js
+++ dist/js/lineas.js
@@ -1,3 +1,3 @@
-d3.svg("svg/obras_arte_graf.svg").then(function (datossvg) {
+d3.svg("svg/Arrow_Sexenio_rev.svg").then(function (datossvg) {
   d3.select(".grafico").node().append(datossvg.documentElement);
 });
