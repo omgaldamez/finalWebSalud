fde42e1df87baedea071aea9df98cf980c90a3f1
omgaldamez <127152662+omgaldamez@users.noreply.github.com>
2023-04-13 16:36:29 -0600

    2 columnas con SVG

--- dist/js/lineas.js
+++ dist/js/lineas.js
@@ -1,3 +1,7 @@
 d3.svg("svg/Arrow_Sexenio_rev.svg").then(function (datossvg) {
   d3.select(".grafico").node().append(datossvg.documentElement);
 });
+
+d3.svg("svg/Arrow_Sexenio_rev.svg").then(function (datossvg) {
+  d3.select(".grafico2").node().append(datossvg.documentElement);
+});
