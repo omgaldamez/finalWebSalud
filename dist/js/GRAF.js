9559c0e413f44da1c65d98dc5a5e6256cbb572a2
omgaldamez <127152662+omgaldamez@users.noreply.github.com>
2023-04-16 00:45:24 -0600

    Tabla y siblings Arrow

--- dist/js/GRAF.js
+++ dist/js/GRAF.js
@@ -19,14 +19,33 @@ d3.json(endpoint_ArrowPIB).then((datosjson) => {
     Entidad.push(data.ENTIDAD);
   });
 
-  d3.select(".grafico").on("mouseenter", function () {
-      d3.select(".grafico").attr("data-OrdenGraf", Entidad);
+      d3.selectAll("#Estados text").on("click", function () {
+        let estadoSeleccionado = this.innerHTML;
 
-      d3.selectAll("#Arrow g").on("mouseenter", function () {
-        d3.select("#TC1").text("Ranking: " + Entidad[0]);
-        d3.select("#TC2").text("HOVER " + Entidad[2]);
-        d3.select("#TC3").text("HOVER " + Entidad[3]);
-        d3.select("#TC4").text("HOVER " + Entidad[5]);
+
+        function searchSiblings(searchString, columnName) {
+          for (let i = 0; i < datosjson.data.length; i++) {
+            if (datosjson.data[i][columnName] === searchString) {
+              return datosjson.data[i];
+            }
+          }
+          return null;
+        }
+const match = searchSiblings(estadoSeleccionado, "ENTIDAD");
+console.log("match: ",match); // prints {index: 0, ENTIDAD: "AGS", S1: "2.32%", S2: "2.79%", S3: "3.11%", S4: "2.93%", S1vsS2: "20%", S1vsS3:
+console.log("match index: ",match.S2); // prints {index: 0, ENTIDAD: "AGS", S1: "2.32%", S2: "2.79%", S3: "3.11%", S4: "2.93%", S1vsS2: "20%", S1vsS3:
+
+
+
+
+
+
+
+d3.select("#TC1").text("Estado: " + match.NOMBRE);
+        d3.select("#TC2").text("index " + match.index);
+        d3.select("#TC3").text("S1 " + match.S1);
+        d3.select("#TC4").text("S2 " + match.S2);
+        d3.select("#TC5").text("S1vsS2% " + match.S1vsS2);
         //d3.select(this).attr("transform", "translate(0,-150)");
 
       });
@@ -42,7 +61,6 @@ d3.json(endpoint_ArrowPIB).then((datosjson) => {
     console.log("ENTRA texto");
       });
 
-  });
 
   d3.selectAll(".grafico text").on("mouseenter", function () {
     d3.select(this).style("fill", "#FFFFFF");
@@ -67,21 +85,5 @@ console.log("filtro: ", siblings)
 console.log("filtro2: ", siblings.index)
 console.log("filtro2: ", siblings[0].index)
 
-
-function searchSiblings(searchString, columnName) {
-  for (let i = 0; i < datosjson.data.length; i++) {
-    if (datosjson.data[i][columnName] === searchString) {
-      return datosjson.data[i];
-    }
-  }
-  return null; // return null if no match is found
-}
-
-// Example usage
-const match = searchSiblings("2.32%", "S1");
-console.log("match: ",match); // prints {index: 0, ENTIDAD: "AGS", S1: "2.32%", S2: "2.79%", S3: "3.11%", S4: "2.93%", S1vsS2: "20%", S1vsS3:
-console.log("match index: ",match.index); // prints {index: 0, ENTIDAD: "AGS", S1: "2.32%", S2: "2.79%", S3: "3.11%", S4: "2.93%", S1vsS2: "20%", S1vsS3:
-
 });
-
 
