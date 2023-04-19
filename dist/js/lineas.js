da5df2ff6de07d63ae2b1ce6264c8ce45b07ffe9
omgaldamez <127152662+omgaldamez@users.noreply.github.com>
2023-04-18 20:34:11 -0600

    grafico S Mex

--- dist/js/lineas.js
+++ dist/js/lineas.js
@@ -2,3 +2,17 @@ d3.svg("svg/Arrow_Sexenio_rev.svg").then(function (datossvg) {
   d3.select(".grafico").node().append(datossvg.documentElement);
 });
 
+d3.svg("svg/SVG_GASTO.svg").then(function (datossvg) {
+  d3.select(".graficoMexS").node().append(datossvg.documentElement);
+});
+
+d3.svg("svg/SVG_GASTO.svg").then(function (datossvg) {
+  d3.selectAll(".x path").style("fill", "none");
+  d3.selectAll(".azules path").style("fill", "none");
+  d3.selectAll(".DOTS path").style("fill", "none");
+  d3.selectAll(".textoANIO tspan").style("fill", "none");
+  d3.selectAll(".textos tspan").style("fill", "none");
+  d3.selectAll("#NUM textPath").attr("startOffset", "40%");
+  d3.selectAll(".infoHover text").style("fill", "none");
+  d3.selectAll(".infoError text").style("fill", "none");
+});
